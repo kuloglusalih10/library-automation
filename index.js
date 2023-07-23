3aefb522cabcbaf566ee83d4044699b6480a2462
Salih Kuloğlu <kuloglusalih10@gmail.com>
2023-07-23 22:53:40 +0300

    navbar

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ require("dotenv").config();
 const router = express.Router();
 const app = express();
 const main_routes = require("./routes/main_routes");
+const path = require("path");
 
 app.set("view-engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(process.env.PORT, () => {
   console.log("Server Çalışmaya Başladı");
