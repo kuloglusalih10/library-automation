7f12ca76b3fc6c3add297a036e6adb587c19a4d2
AlihanDemirdas <info.alihandemirdas@gmail.com>
2023-07-24 13:35:51 +0300

    devam.

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const router = express.Router();
 const app = express();
 const main_routes = require("./routes/main_routes");
 const path = require("path");
+const localtunnel = require("localtunnel");
 
 app.set("view-engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist")));
 app.use(express.static(path.join(__dirname, "public")));
+
+(async () => {
+
+   const tunnel = await localtunnel({ port: 3000 });
+   console.log(tunnel.url);
+   }
+)();
 
 app.listen(process.env.PORT, () => {
   console.log("Server Çalışmaya Başladı");
